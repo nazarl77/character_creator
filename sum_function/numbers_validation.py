d18eecd0f1d457bb78cf79a633b0f2026fef83ee
Nazar Latushynskyi <naz.reastwood@gmail.com>
2026-03-25 15:17:01 +0000

    Updated sum function

--- sum_function/numbers_validation.py
+++ sum_function/numbers_validation.py
@@ -1,21 +1,49 @@
-def num_validation(nums):
-    return sum(nums)
+def num_summing(nums,amount):
+    i = 0
+    total = 0
 
-print('Lets sum up your 5 numbers!\n')
+    while i != amount:
+        total += nums[i]
+        i += 1
 
-numbers = []
-while len(numbers) != 5:
-    num = input('Enter a number:')
-    if not num.isdigit():
-        print('Program accepts only numbers.Try again. ')
+    return total
+
+def howmany():
+    num = ''
+    while True:
+        num = input('How many numbers you want to sum up:')
         
-    else:
-        num = int(num)
-        numbers.append(num) 
-        
-   
-sum = num_validation(numbers)
-print('Summary:' , sum)
+        if num.isdigit():
+            number = int(num)
+            if number > 0:
+                return number
+            else:
+                print('Number must be greater than 0.Try again.')
+        else:
+            print('Program accepts only whole numbers.')        
+       
+
+def list_create(amount):       
+     numbers = []
+     while len(numbers) != amount:
+         num = input('Enter a number:')
+         if not num.isdigit():
+            print('Program accepts only numbers.Try again. ')       
+         else:
+            num = int(num)
+            numbers.append(num) 
+     return numbers
+           
 
 
+def main():
+    print('Lets sum up your numbers!')
+
+    amount = howmany()
+    numbers = list_create(amount)
+    total = num_summing(numbers , amount)
+    
+    print('Summary:' , total)
+
+main()
 
